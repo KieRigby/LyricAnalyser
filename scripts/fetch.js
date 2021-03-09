8e3f0f735573f2ab07e7b7390591c3144e889d64
Kieran Rigby <kieran.rigby@focusrite.com>
2021-03-09 02:26:26 +0000

    Added client credential flow into the script

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,6 +1,25 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// If running in a development environment fetch keys from .env file
 if (process.env.NODE_ENV == "development")
     require('dotenv').config()
 
+var spotify = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_API_ID,
+    clientSecret: process.env.SPOTIFY_API_SECRET,
+});
 
-console.log(process.env.SPOTIFY_API_ID)
-console.log(process.env.SPOTIFY_API_SECRET)
+//Use client credential flow to obtain API access token.
+spotify.clientCredentialsGrant().then(
+    function(data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+        spotify.setAccessToken(data.body['access_token']);
+
+        
+
+    },
+    function(err) {
+      console.log('Something went wrong when retrieving an access token', err);
+    }
+  );
