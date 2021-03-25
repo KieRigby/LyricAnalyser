373995e77b7e8911e605e9c3a3c525f7174a45e0
Kieran Rigby <kieran.rigby@focusrite.com>
2021-03-25 01:11:57 +0000

    Refactored into async and await functions and reduced number of spotify API calls

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -11,130 +11,178 @@ const spotify = new SpotifyWebApi({
     clientSecret: process.env.SPOTIFY_API_SECRET,
 });
 
-const csvWriter = createCsvWriter({
-    path: 'output.csv',
-    header: [
-        {id: 'artist', title: 'ARTIST'},
-        {id: 'explicit', title: 'EXPLICIT'},
-        {id: 'id', title: 'ID'},
-        {id: 'name', title: 'NAME'},
-        {id: 'popularity', title: 'POPULARITY'},
-        {id: 'danceability', title: 'DANCEABILITY'},
-        {id: 'energy', title: 'ENERGY'},
-        {id: 'loudness', title: 'LOUDNESS'},
-        {id: 'speechiness', title: 'SPEECHINESS'},
-        {id: 'acousticness', title: 'ACOUSTICNESS'},
-        {id: 'instrumentalness', title: 'INSTRUMENTALNESS'},
-        {id: 'liveness', title: 'LIVENESS'},
-        {id: 'valence', title: 'VALENCE'},
-        {id: 'tempo', title: 'TEMPO'},
-        {id: 'duration_ms', title: 'DURATION_MS'},
-        {id: 'genres', title: 'GENRES'},       
-        {id: 'lyrics', title: 'LYRICS'}       
-    ]
-});
+// const csvWriter = createCsvWriter({
+//     path: 'output.csv',
+//     header: [
+//         {id: 'artist', title: 'ARTIST'},
+//         {id: 'explicit', title: 'EXPLICIT'},
+//         {id: 'id', title: 'ID'},
+//         {id: 'name', title: 'NAME'},
+//         {id: 'popularity', title: 'POPULARITY'},
+//         {id: 'danceability', title: 'DANCEABILITY'},
+//         {id: 'energy', title: 'ENERGY'},
+//         {id: 'loudness', title: 'LOUDNESS'},
+//         {id: 'speechiness', title: 'SPEECHINESS'},
+//         {id: 'acousticness', title: 'ACOUSTICNESS'},
+//         {id: 'instrumentalness', title: 'INSTRUMENTALNESS'},
+//         {id: 'liveness', title: 'LIVENESS'},
+//         {id: 'valence', title: 'VALENCE'},
+//         {id: 'tempo', title: 'TEMPO'},
+//         {id: 'duration_ms', title: 'DURATION_MS'},
+//         {id: 'genres', title: 'GENRES'},       
+//         {id: 'lyrics', title: 'LYRICS'}       
+//     ]
+// });
 
-//Use client credential flow to obtain API access token.
-spotify.clientCredentialsGrant().then(
-    function(data) {
-        console.log('The access token expires in ' + data.body['expires_in']);
-        console.log('The access token is ' + data.body['access_token']);
-        spotify.setAccessToken(data.body['access_token']);
+const mainFunction = () => {
+    login (authenticated);
+};
 
-        // Get available genre seeds
-        spotify.getAvailableGenreSeeds()
-        .then(function(data) {
-            let genres = data.body.genres;
-
-            // Remove genres which aren't to be processed (eg acoustic)
-            
-            spotify.searchPlaylists(genres[0])
-            .then(function(data) {
-                let playlistID = data.body.playlists.items[0].id;
-
-                spotify.getPlaylistTracks(playlistID, {
-                    fields: "items.track(id, name, explicit, popularity, artists(name, id))"
-                }).then(function(data){
-                    // fetch: id, name, explicitness, popularity, artists
-                    let tracks = data.body.items.map((item)=>{return item.track})
-                    // console.log(tracks)
-                    let trackIDs = tracks.map((track)=>{return track.id})
-
-                    /* Get Audio Features for several tracks */
-                    spotify.getAudioFeaturesForTracks(trackIDs)
-                    .then(function(data) {
-                        features = data.body.audio_features;
-                        tracks.forEach((track, index) => {
-                            tracks[index].danceability = features[index].danceability
-                            tracks[index].energy = features[index].energy
-                            tracks[index].loudness = features[index].loudness
-                            tracks[index].speechiness = features[index].speechiness
-                            tracks[index].acousticness = features[index].acousticness
-                            tracks[index].instrumentalness = features[index].instrumentalness
-                            tracks[index].liveness = features[index].liveness
-                            tracks[index].valence = features[index].valence
-                            tracks[index].tempo = features[index].tempo
-                            tracks[index].duration_ms = features[index].duration_ms
-
-                            let artistIDs = track.artists.map((artist) => {return artist.id})
-
-                            spotify.getArtists(artistIDs)
-                            .then(function(data){
-                                let unmerged_genres = data.body.artists.map((artist) => {return artist.genres});
-                                let genres = [].concat.apply([], unmerged_genres);
-                                tracks[index].genres = genres
-
-                                const options = {
-                                    apiKey: process.env.GENIUS_API_TOKEN,
-                                    title: track.name,
-                                    artist: track.artists[0].name,
-                                    optimizeQuery: false
-                                };
-                                
-                                Genius.getLyrics(options)
-                                .then(function(lyrics){
-                                    tracks[index].lyrics = lyrics;
-                                    tracks[index].artist = tracks[index].artists[0].name
-                                    delete tracks[index].artists
-
-                                    console.log(tracks[index].artist)
-
-                                    if(index == tracks.length - 1) {
-                                        csvWriter.writeRecords(tracks)
-                                        .then(() => {
-                                            console.log('...LOL');
-                                        });
-                                    }
-
-                                    setTimeout(() => {  console.log("Waiting!"); }, 5000);
-                                    
-                                }, function (err){
-                                    console.error("Something went wrong getting the lyrics", err)
-                                });
-
-                            }, function(err){
-                                console.error("Something went wrong getting the artist's genre", err)
-                            })
-                        })
-                    }, function(err) {
-                        console.error("Something went wrong getting audio features for tracks in playlist", err);
-                    });
-                    
-                }, function(err) {
-                    console.error("Something went wrong getting tracks from playlist", err)
-                });  
-
-            }, function(err) {
-                console.error('Something went wrong searching for genre playlist!', err);
-            });
-
-        }, function(err) {
-            console.error('Something went wrong retrieving genres!', err);
-        });
-    },
-    function(err) {
-      console.error('Something went wrong when retrieving an access token', err);
+const login = async (callback) => {
+    try{
+        const data = await spotify.clientCredentialsGrant();
+        callback (data);
+    } catch (err) {
+        console.error ("Error authenticating user!", err);
     }
-);
-    
+};
 
+const authenticated = data => {
+    console.log ('The access token expires in ' + data.body['expires_in']);
+    console.log ('The access token is ' + data.body['access_token']);
+    spotify.setAccessToken (data.body['access_token']);
+
+    getGenreList (processGenreList);
+};
+
+const getGenreList = async (callback) => {
+    try{
+        const data = await spotify.getAvailableGenreSeeds();
+        callback (data.body.genres);
+    } catch (err) {
+        console.error ("Error fetching genre list!", err);
+    }
+};
+
+const processGenreList = (genres) => {
+    //remove blacklisted genres
+    //loop over all genres
+
+    getPlaylist (genres[1], processPlaylist)
+};
+
+const getPlaylist = async (genre, callback) => {
+    try{
+        const data = await spotify.searchPlaylists (genre)
+        callback (data.body.playlists.items[0].id);
+    } catch (err) {
+        console.error ("Error fetching playlist!", err);
+    }
+};
+
+const processPlaylist = (playlist) =>{
+    getPlaylistTracks(playlist, processPlaylistTracks)
+};
+
+const getPlaylistTracks = async (playlistID, callback) => {
+    try{
+        const data = await spotify.getPlaylistTracks(playlistID, { limit:50, fields: "items.track(id, name, explicit, popularity, artists(name, id))"});
+        callback (data.body.items.map((item)=>{return item.track}));
+    } catch (err) {
+        console.error ("Error fetching tracks from playlist!", err);
+    }
+};
+
+const processPlaylistTracks = (tracks) => {
+    getAudioFeatures(tracks, processAudioFeaturesForTracks)
+};
+
+const getAudioFeatures = async (tracks, callback) => {
+    let trackIDs = tracks.map((track)=>{return track.id})
+    try{
+        const data = await spotify.getAudioFeaturesForTracks(trackIDs);
+        callback (data.body.audio_features, tracks);
+    } catch (err) {
+        console.error ("Error fetching audio features for tracks", err);
+    }
+};
+
+const processAudioFeaturesForTracks = (features, tracks) => {
+    tracks.forEach((track, index) => {
+        track.danceability = features[index].danceability
+        track.energy = features[index].energy
+        track.loudness = features[index].loudness
+        track.speechiness = features[index].speechiness
+        track.acousticness = features[index].acousticness
+        track.instrumentalness = features[index].instrumentalness
+        track.liveness = features[index].liveness
+        track.valence = features[index].valence
+        track.tempo = features[index].tempo
+        track.duration_ms = features[index].duration_ms
+        track.main_artist = track.artists[0]
+        delete track.artists
+    });
+
+    getGenreOfArtists(tracks, processGenresFromArtists)
+};
+
+const getGenreOfArtists = async (tracks, callback) => {
+    let artistIDs = tracks.map((track)=>{return track.main_artist.id});
+    try{
+        const data = await spotify.getArtists(artistIDs);
+        let genres = data.body.artists.map((artist) => {return artist.genres});
+        callback (genres, tracks);
+    } catch (err) {
+        console.error ("Error fetching genres from artists", err);
+    }
+};
+
+const processGenresFromArtists = (genres, tracks) => {
+    tracks.forEach((track, index) => {
+        tracks[index].genres = genres[index]
+    });
+    getLyrics(tracks, processLyrics);
+};
+
+const getLyrics = async (tracks, callback) => {
+    track_options = [];
+    tracks.forEach((track, index) => {
+        track_options.push({
+            apiKey: process.env.GENIUS_API_TOKEN,
+            title: track.name,
+            artist: track.main_artist.name,
+            optimizeQuery: true
+        });
+    });
+        
+    const results = await Promise.all(track_options.map(Genius.getSong));
+
+    let lyrics = results.map((result) => {
+        if(result === null || !result.url.includes("lyrics")){
+            return null;
+        }
+    
+        return result.lyrics;
+    });
+    
+    callback(tracks, lyrics)
+};
+
+const processLyrics = (tracks, lyrics) => {
+
+    tracks.forEach((track, index)=>{
+        track.lyrics = lyrics[index];
+    });
+
+    tracks = tracks.filter((track) => {
+        return (track.lyrics !== null)
+    });
+    console.log(tracks)
+
+
+    // console.log(tracks);
+
+    //write to file
+};
+
+mainFunction();
